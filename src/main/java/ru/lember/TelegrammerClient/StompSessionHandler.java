16d58fe0ffefdf40defb64332c96c1afb46bf3db
Алексей Лемберский <alexlember@gmail.com>
2019-11-06 01:55:42 +0300

    3 - something entered

--- src/main/java/ru/lember/TelegrammerClient/StompSessionHandler.java
+++ src/main/java/ru/lember/TelegrammerClient/StompSessionHandler.java
@@ -1,9 +1,13 @@
 package ru.lember.TelegrammerClient;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.messaging.support.GenericMessage;
 
 import java.lang.reflect.Type;
 
@@ -53,8 +57,12 @@ public class StompSessionHandler extends StompSessionHandlerAdapter {
 
     @Override
     public void afterConnected(StompSession session, StompHeaders stompHeaders) {
+        log.info("Subscribed : " + session);
+
         session.subscribe("/topic/messages", this);
-        session.send("/app/chat", getSampleMessage());
+
+        this.session = session;
+        //session.send("/app/chat", getSampleMessage()); // почему-то отправляются только из subscribe
     }
 
     private Response getSampleMessage() {
@@ -63,12 +71,24 @@ public class StompSessionHandler extends StompSessionHandlerAdapter {
 
     @Override
     public Type getPayloadType(StompHeaders stompHeaders) {
-        return Response.class;
+        return Request.class;
     }
 
     @Override
     public void handleFrame(StompHeaders stompHeaders, Object payload) {
-        Response msg = (Response) payload;
-        log.info("Received : " + msg.getCmd() + " id : " + msg.getRequestId());
+        Request request = (Request) payload;
+        log.info("Received : " + request.getCmd() + " id : " + request.getId());
+
+        Response response = new Response(request.getId(), request.getCmd(), "Reply message");
+        Message<Response> message = new GenericMessage<>(response);
+        //session.send("/topic/messages", response);
+        session.send("/app/topic/messages/reply", response);
+    }
+
+    @Override
+    public void handleException(StompSession session, @Nullable StompCommand command,
+                                StompHeaders headers, byte[] payload, Throwable exception) {
+
+        log.error("Error: ", exception);
     }
 }
